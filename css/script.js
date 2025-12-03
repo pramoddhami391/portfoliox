a84ff8f14c66796e3f85537fb9fbee836186106a
Pramod Dhami <168343845+pramoddhami391@users.noreply.github.com>
2025-12-03 14:01:57 +0545

    path change

--- css/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", (event) => {
- event.preventDefault();
- alert("You are gay");
-}
-   );
---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", (event) => {
+ event.preventDefault();
+ alert("You are gay");
+}
+   );

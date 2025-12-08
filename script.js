bf86fd1559516f282f33b6d92884da79351b2408
Pramod Dhami <168343845+pramoddhami391@users.noreply.github.com>
2025-12-08 16:37:38 +0545

    changes on the project section

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const buttons = document.querySelectorAll(".view-more-btn");
 buttons.forEach(btn => {
   btn.addEventListener("click", (event) => {
     event.preventDefault(); // stops page from reloading for <a>
-    alert("You are gay!");
+    alert("This section is on work.");
   });
 });

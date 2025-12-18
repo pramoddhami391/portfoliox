d212772ff64d9f42950093c4bde08ca6a43128c5
Pramod Dhami <168343845+pramoddhami391@users.noreply.github.com>
2025-12-18 20:35:41 +0545

    Project section updated and made responsive

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const buttons = document.querySelectorAll(".view-more-btn");
 buttons.forEach(btn => {
   btn.addEventListener("click", (event) => {
     event.preventDefault(); // stops page from reloading for <a>
-    alert("This section is on work.");
+    window.open("https://www.pramoddhami.com.np/", "_blank");
   });
 });

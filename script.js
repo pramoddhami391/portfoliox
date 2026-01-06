9f962b83283ae0c4e368995009bb783a72806851
Pramod Dhami <168343845+pramoddhami391@users.noreply.github.com>
2026-01-06 18:20:10 +0545

    noopener,noreferer used in js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const taskManagerBtn=document.getElementById("taskManagerBtn")
 
 portfolioBtn.addEventListener("click",(event)=>{ 
   event.preventDefault(); // stops page from reloading for <a>
-    window.open("https://www.pramoddhami.com.np/", "_blank");}
+    window.open("https://www.pramoddhami.com.np/", "_blank",'noopener,noreferrer');}
   
 )
 taskManagerBtn.addEventListener("click",(event)=>{
    event.preventDefault(); // stops page from reloading for <a>
-    window.open("https://task-manager-flaskproject-1.onrender.com/", "_blank");
+    window.open("https://task-manager-flaskproject-1.onrender.com/", "_blank",'noopener,noreferrer');
 })

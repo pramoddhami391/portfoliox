d558470b3709086a33e8270f0dad0f16d93d8d8b
Pramod Dhami <168343845+pramoddhami391@users.noreply.github.com>
2026-01-30 08:53:38 +0545

    updated the skills  section and deleted the save.php cuz it vercel does;t support php

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ taskManagerBtn.addEventListener("click",(event)=>{
    event.preventDefault(); // stops page from reloading for <a>
     window.open("https://task-manager-flaskproject-1.onrender.com/", "_blank",'noopener,noreferrer');
 })
+
+
+// js for skill section
+// Select all skill cards
+const skillCards = document.querySelectorAll('.skill-card');
+
+skillCards.forEach(card => {
+  const header = card.querySelector('.skill-header');
+  
+  header.addEventListener('click', () => {
+    card.classList.toggle('active');
+  });
+});
+
+//js for contact form
+// Select the contact form
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function(e) {
+  e.preventDefault(); // prevent default form submission
+
+  // Optional: Do something here (like send data via AJAX)
+
+  // Reload the page
+  window.location.reload();
+});
